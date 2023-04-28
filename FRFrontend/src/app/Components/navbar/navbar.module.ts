6cac2ee772a770f10428cc437558fab8fd579a4e
Rashmika9712 <rashmikamadushansamarakoon@gmail.com>
2023-04-28 14:29:50 +0530

    Update fronend

---
+++ FRFrontend/src/app/Components/navbar/navbar.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatToolbarModule, MatButtonModule} from '@angular/material';
+
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent
+  ],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule
+  ],
+  exports:[
+    NavbarComponent
+  ]
+})
+export class NavbarModule { }
--- FRFrontend/src/app/app.module.ts
+++ FRFrontend/src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './Components/navbar/navbar.component';
+import { NavbarModule } from './Components/navbar/navbar.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NavbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
