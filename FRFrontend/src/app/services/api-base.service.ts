c1e0115ce3c3b30b8ccf30be7080ef7b70631a87
Rashmika9712 <rashmikamadushansamarakoon@gmail.com>
2023-04-29 18:51:55 +0530

    Update base API service

--- FRFrontend/src/app/services/api-base.service.ts
+++ FRFrontend/src/app/services/api-base.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiBaseService {
 
-  constructor() { }
+  readonly baseUrl = 'https://localhost:44333/api/';
+
+  constructor(private http: HttpClient) { }
+
+  public getAll<T>(){
+    return this.http.get<T>(this.baseUrl);
+  }
+
+  public get<T>(id:number, API: string){
+    return this.http.get(this.baseUrl+API+"/"+id);
+  }
+
+  public create<T>(
+    requestModel: any
+  ){
+    return this.http.post<T>(this.baseUrl, requestModel);
+  }
+
+  public update<T>(
+    id: number,
+    requestModel: any
+  ){    
+    return this.http.put<T>(this.baseUrl, requestModel);
+  }
+
+  public delete(id: number){
+    return this.http.delete(this.baseUrl);
+  }
 }
