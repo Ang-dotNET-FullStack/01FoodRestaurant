af413ebf8b2fe1f2e8edab596fc46f6046ff2f86
Rashmika9712 <rashmikamadushansamarakoon@gmail.com>
2023-05-10 20:17:08 +0530

    update MenuItem service

--- FRFrontend/src/app/Components/menu-item/menu-item.service.ts
+++ FRFrontend/src/app/Components/menu-item/menu-item.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { MenuItem } from 'src/app/Models/MenuItem.model';
+import { MenuItemApiService } from 'src/app/services/API/menu-item-api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuItemService {
 
-  constructor() { }
+  constructor
+  (
+    private menuItemApiService: MenuItemApiService
+  ) { }
+
+  public getMenuItems(){
+    return this.menuItemApiService.getAllMenuItems();
+  }
+
+  public createMenuItem(menuItem: MenuItem){
+    return this.menuItemApiService.createMenuItem(menuItem);
+  }
+
+  public updateMenuItem(id: number, menuItem: MenuItem){
+    return this.menuItemApiService.updateMenuItem(id, menuItem);
+  }
+  public deleteCatgory(id: number){
+    return this.menuItemApiService.deleteMenuItem(id);
+  }
+
+  public reloadPage(){
+    window.location.reload();
+  }
 }
